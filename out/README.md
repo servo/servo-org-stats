80b3cd1a61c8fdf43da385ad79a828a8515204d1
edunham <edunham@mozilla.com>
2015-11-30 15:57:33 -0800

    Initial commit of standalone gitstat+orglog

---
+++ out/README.md
@@ -0,0 +1,7 @@
+Servo Org Stats
+--------------
+
+These statistics were generated with
+[gitstat](https://github.com/youknowone/gitstat) and
+[orglog](https://github.com/edunham/orglog), then deployed here by a TravisCI
+build on the orglog repo triggered by [nightli.es](http://nightli.es/)
---
+++ out/forcepush.sh
@@ -0,0 +1,14 @@
+cd out
+git init
+git config user.name "Travis CI"
+git config user.email "travis-auto-deploy@edunham.net"
+git add index.html 
+git add README.md
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be
+# exposed.
+git push --force --quiet "https://${GH_TOKEN}@github.com/edunham/servo-org-stats" master:gh-pages > /dev/null 2>&1
