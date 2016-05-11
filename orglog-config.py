5295c502cdecae158671c1746fd21734d6244c56
edunham <edunham@mozilla.com>
2016-05-11 15:40:21 -0700

    link repo and keep orglog config local

---
+++ orglog-config.py
@@ -0,0 +1,12 @@
+org = "servo"
+
+ignore_repos = ["skia", "skia-snapshots", "cairo"]
+
+# Path to where we'll dump the bare checkouts. Must end in /
+clones_dir = "repos/"
+
+# Path to the concatenated log
+log_path = "log.txt"
+
+# Nuke the clones_dir afterwards?
+destroy_clones = True
